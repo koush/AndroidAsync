7b7540379004272830dab781ab442d4870fbb7a2
Koushik Dutta <koushd@gmail.com>
2012-08-24 09:39:47 -0700

    sample

---
+++ AndroidAsyncSample/src/com/koushikdutta/async/test/MainActivity.java
@@ -0,0 +1,20 @@
+package com.koushikdutta.async.test;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+}
