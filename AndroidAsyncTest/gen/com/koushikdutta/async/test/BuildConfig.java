bd8a970acd46231fe53a225b02684a34aedbdecd
Koushik Dutta <koushd@gmail.com>
2013-05-16 09:29:31 -0700

    remove gen

--- AndroidAsyncTest/gen/com/koushikdutta/async/test/BuildConfig.java
+++
@@ -1,8 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-/** Automatically generated file. DO NOT MODIFY */
-package com.koushikdutta.async.test;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
