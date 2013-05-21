96f890074d576101b3bde261f747962c0e70ec79
Koushik Dutta <koushd@gmail.com>
2013-05-20 22:23:11 -0700

    Revert "remove extraneous arg"
    This reverts commit 02279b09194bec46fa818196a3c641e39f2a312f.

--- AndroidAsync/src/com/koushikdutta/async/callback/RequestCallback.java
+++ AndroidAsync/src/com/koushikdutta/async/callback/RequestCallback.java
@@ -2,7 +2,7 @@ package com.koushikdutta.async.callback;
 
 import com.koushikdutta.async.http.AsyncHttpResponse;
 
-public interface RequestCallback<T> extends ResultCallback<T> {
+public interface RequestCallback<T> extends ResultCallback<AsyncHttpResponse, T> {
     public void onConnect(AsyncHttpResponse response);
     public void onProgress(AsyncHttpResponse response, int downloaded, int total);
 }
