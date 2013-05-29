bdeba4bdf93abd87ff76b5cfe6fa61ed78eeb2cf
Koushik Dutta <koushd@gmail.com>
2013-05-29 00:04:49 -0700

    test data

---
+++ AndroidAsync/src/com/koushikdutta/async/DataTrackingEmitter.java
@@ -0,0 +1,13 @@
+package com.koushikdutta.async;
+
+/**
+ * Created by koush on 5/28/13.
+ */
+public interface DataTrackingEmitter extends DataEmitter {
+    public interface DataTracker {
+        void onData(int totalBytesRead);
+    }
+    void setDataTracker(DataTracker tracker);
+    DataTracker getDataTracker();
+    int getBytesRead();
+}
--- AndroidAsync/src/com/koushikdutta/async/FilteredDataEmitter.java
+++ AndroidAsync/src/com/koushikdutta/async/FilteredDataEmitter.java
@@ -4,7 +4,7 @@ import com.koushikdutta.async.callback.CompletedCallback;
 import com.koushikdutta.async.callback.DataCallback;
 import com.koushikdutta.async.wrapper.DataEmitterWrapper;
 
-public class FilteredDataEmitter extends DataEmitterBase implements DataEmitter, DataCallback, DataEmitterWrapper {
+public class FilteredDataEmitter extends DataEmitterBase implements DataEmitter, DataCallback, DataEmitterWrapper, DataTrackingEmitter {
     DataEmitter mEmitter;
     @Override
     public DataEmitter getDataEmitter() {
@@ -24,10 +24,33 @@ public class FilteredDataEmitter extends DataEmitterBase implements DataEmitter,
             }
         });
     }
-    
+
+    @Override
+    public int getBytesRead() {
+        return totalRead;
+    }
+
+    @Override
+    public DataTracker getDataTracker() {
+        return tracker;
+    }
+
+    @Override
+    public void setDataTracker(DataTracker tracker) {
+        this.tracker = tracker;
+    }
+
+    DataTracker tracker;
+    int totalRead;
     @Override
     public void onDataAvailable(DataEmitter emitter, ByteBufferList bb) {
+        if (bb != null)
+            totalRead += bb.remaining();
         Util.emitAllData(this, bb);
+        if (bb != null)
+            totalRead -= bb.remaining();
+        if (tracker != null && bb != null)
+            tracker.onData(totalRead);
         // if there's data after the emitting, and it is paused... the underlying implementation
         // is obligated to cache the byte buffer list.
     }
