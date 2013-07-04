a384cae440f7dff81ae66ba45538c5419489555b
Anup Cowkur <anup.cowkur@ymedialabs.com>
2013-07-04 15:53:54 +0530

    Added public method to set your own SSL hostname verifier.

--- AndroidAsync/src/com/koushikdutta/async/http/AsyncSSLSocketMiddleware.java
+++ AndroidAsync/src/com/koushikdutta/async/http/AsyncSSLSocketMiddleware.java
@@ -1,13 +1,13 @@
 package com.koushikdutta.async.http;
 
-import java.net.URI;
-
 import com.koushikdutta.async.AsyncSSLSocketWrapper;
 import com.koushikdutta.async.AsyncSocket;
 import com.koushikdutta.async.callback.ConnectCallback;
 
+import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
+import java.net.URI;
 
 public class AsyncSSLSocketMiddleware extends AsyncSocketMiddleware {
     public AsyncSSLSocketMiddleware(AsyncHttpClient client) {
@@ -21,8 +21,15 @@ public class AsyncSSLSocketMiddleware extends AsyncSocketMiddleware {
     }
 
     TrustManager[] trustManagers;
+
     public void setTrustManagers(TrustManager[] trustManagers) {
         this.trustManagers = trustManagers;
+    }
+
+    HostnameVerifier hostnameVerifier;
+
+    public void setHostnameVerifier(HostnameVerifier hostnameVerifier) {
+        this.hostnameVerifier = hostnameVerifier;
     }
 
     @Override
@@ -31,9 +38,8 @@ public class AsyncSSLSocketMiddleware extends AsyncSocketMiddleware {
             @Override
             public void onConnectCompleted(Exception ex, AsyncSocket socket) {
                 if (ex == null) {
-                    callback.onConnectCompleted(ex, new AsyncSSLSocketWrapper(socket, uri.getHost(), port, sslContext, trustManagers, true));
-                }
-                else {
+                    callback.onConnectCompleted(ex, new AsyncSSLSocketWrapper(socket, uri.getHost(), port, sslContext, trustManagers, hostnameVerifier, true));
+                } else {
                     callback.onConnectCompleted(ex, socket);
                 }
             }
