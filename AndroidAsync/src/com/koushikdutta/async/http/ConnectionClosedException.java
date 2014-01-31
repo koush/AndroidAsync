36b00cad6c0c8d7902538cade56e9c0d49eda78d
Dmitry Apresian <dmitry.apresian@jivesoftware.com>
2014-01-31 09:54:46 -0800

    Pass the original exception as the cause when throwing ConnectionClosedException

--- AndroidAsync/src/com/koushikdutta/async/http/ConnectionClosedException.java
+++ AndroidAsync/src/com/koushikdutta/async/http/ConnectionClosedException.java
@@ -4,4 +4,8 @@ public class ConnectionClosedException extends Exception {
     public ConnectionClosedException(String message) {
         super(message);
     }
+
+    public ConnectionClosedException(String detailMessage, Throwable throwable) {
+        super(detailMessage, throwable);
+    }
 }
