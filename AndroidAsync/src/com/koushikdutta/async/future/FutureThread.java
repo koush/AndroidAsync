50afaa58b44cb452e19efa7e4b5bfc0288536e1b
Koushik Dutta <koushd@gmail.com>
2014-08-08 01:32:53 -0700

    use thread name

--- AndroidAsync/src/com/koushikdutta/async/future/FutureThread.java
+++ AndroidAsync/src/com/koushikdutta/async/future/FutureThread.java
@@ -19,6 +19,6 @@ public class FutureThread<T> extends SimpleFuture<T> {
                     setComplete(e);
                 }
             }
-        }).start();
+        }, name).start();
     }
 }
