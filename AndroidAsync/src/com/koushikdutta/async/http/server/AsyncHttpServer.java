4ee0ae0e6206f90a07f3a2269abc6c655a0289df
paulpdaniels <paulpdaniels@gmail.com>
2014-09-08 18:06:31 -0700

    Added protected API for WebSocket creation

--- AndroidAsync/src/com/koushikdutta/async/http/server/AsyncHttpServer.java
+++ AndroidAsync/src/com/koushikdutta/async/http/server/AsyncHttpServer.java
@@ -318,9 +318,13 @@ public class AsyncHttpServer {
                     response.end();
                     return;
                 }
-                callback.onConnected(new WebSocketImpl(request, response), request.getHeaders());
+                callback.onConnected(createWebSocket(request, response), request.getHeaders());
             }
         });
+    }
+    
+    protected WebSocket createWebSocket(final AsyncHttpServerRequest request, final AsyncHttpServerResponse response) {
+        return new WebSocketImpl(request, response);
     }
     
     public void get(String regex, HttpServerRequestCallback callback) {
