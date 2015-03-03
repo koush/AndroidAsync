e26525dc0e3dc759c550fc79e43eb49330175d4e
Koushik Dutta <koushd@gmail.com>
2015-03-02 23:52:28 -0800

    FutureThread constructor

--- AndroidAsync/src/com/koushikdutta/async/future/FutureThread.java
+++ AndroidAsync/src/com/koushikdutta/async/future/FutureThread.java
@@ -1,11 +1,27 @@
 package com.koushikdutta.async.future;
 
+import java.util.concurrent.ExecutorService;
+
 /**
  * Created by koush on 12/22/13.
  */
 public class FutureThread<T> extends SimpleFuture<T> {
     public FutureThread(final FutureRunnable<T> runnable) {
         this(runnable, "FutureThread");
+    }
+
+    public FutureThread(final ExecutorService pool, final FutureRunnable<T> runnable) {
+        pool.submit(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    setComplete(runnable.run());
+                }
+                catch (Exception e) {
+                    setComplete(e);
+                }
+            }
+        });
     }
 
     public FutureThread(final FutureRunnable<T> runnable, String name) {
