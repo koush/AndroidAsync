e53e1b267ea919cb0ac3e39533ac9de9a4ed7cfc
Koushik Dutta <koushd@gmail.com>
2015-03-07 22:48:48 -1000

    Change AsyncParser.getType to Type and not class.

--- AndroidAsync/src/com/koushikdutta/async/parser/AsyncParser.java
+++ AndroidAsync/src/com/koushikdutta/async/parser/AsyncParser.java
@@ -5,11 +5,13 @@ import com.koushikdutta.async.DataSink;
 import com.koushikdutta.async.callback.CompletedCallback;
 import com.koushikdutta.async.future.Future;
 
+import java.lang.reflect.Type;
+
 /**
  * Created by koush on 5/27/13.
  */
 public interface AsyncParser<T> {
     Future<T> parse(DataEmitter emitter);
     void write(DataSink sink, T value, CompletedCallback completed);
-    Class<T> getType();
+    Type getType();
 }
--- AndroidAsync/src/com/koushikdutta/async/parser/AsyncParserBase.java
+++ AndroidAsync/src/com/koushikdutta/async/parser/AsyncParserBase.java
@@ -1,13 +1,14 @@
 package com.koushikdutta.async.parser;
 
 import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 
 /**
  * Created by koush on 3/6/15.
  */
 public abstract class AsyncParserBase<T> implements AsyncParser<T> {
     @Override
-    public Class<T> getType() {
-        return (Class<T>)((ParameterizedType)getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+    public Type getType() {
+        return ((ParameterizedType)getClass().getGenericSuperclass()).getActualTypeArguments()[0];
     }
 }
