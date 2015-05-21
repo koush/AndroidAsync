ac968b019869acd6ff2e4244756ddab0c6d859c1
Koushik Dutta <koushd@gmail.com>
2015-05-20 18:31:29 -0700

    Add ISO-8859-1 charset.

--- AndroidAsync/src/com/koushikdutta/async/util/Charsets.java
+++ AndroidAsync/src/com/koushikdutta/async/util/Charsets.java
@@ -6,4 +6,5 @@ import java.nio.charset.Charset;
 public class Charsets {
   public static final Charset US_ASCII = Charset.forName("US-ASCII");
   public static final Charset UTF_8 = Charset.forName("UTF-8");
+  public static final Charset ISO_8859_1 = Charset.forName("ISO-8859-1");
 }
