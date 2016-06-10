718f9aba5e0cd6393266c2ffaad41c1cc941f397
Koushik Dutta <koushd@gmail.com>
2016-06-10 12:24:12 -0700

    Apparently MDNS client only applications can query from any port, 5353 not required.
    Also fix reflection related bug that happens in Android N due to internal method protection level change.

--- AndroidAsync/src/com/koushikdutta/async/dns/Dns.java
+++ AndroidAsync/src/com/koushikdutta/async/dns/Dns.java
@@ -103,11 +103,11 @@ public class Dns {
             }
             else {
 //                System.out.println("multicast dns...");
-                dgram = AsyncServer.getDefault().openDatagram(new InetSocketAddress(5353), true);
+                dgram = AsyncServer.getDefault().openDatagram(new InetSocketAddress(0), true);
                 Field field = DatagramSocket.class.getDeclaredField("impl");
                 field.setAccessible(true);
                 Object impl = field.get(dgram.getSocket());
-                Method method = impl.getClass().getMethod("join", InetAddress.class);
+                Method method = impl.getClass().getDeclaredMethod("join", InetAddress.class);
                 method.setAccessible(true);
                 method.invoke(impl, InetAddress.getByName("224.0.0.251"));
                 ((DatagramSocket)dgram.getSocket()).setBroadcast(true);
