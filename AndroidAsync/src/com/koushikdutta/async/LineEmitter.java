e7580a6829a8ecab469ee8b8feb91786f617020b
hongjunmin <hongjunmin@duotin.com>
2017-07-18 17:12:42 +0800

    sometimes the data received is not complete, fix this

--- AndroidAsync/src/com/koushikdutta/async/LineEmitter.java
+++ AndroidAsync/src/com/koushikdutta/async/LineEmitter.java
@@ -1,10 +1,7 @@
 package com.koushikdutta.async;
 
-import android.util.Log;
-
 import com.koushikdutta.async.callback.DataCallback;
 
-import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
 
 public class LineEmitter implements DataCallback {
@@ -35,22 +32,8 @@ public class LineEmitter implements DataCallback {
 
     @Override
     public void onDataAvailable(DataEmitter emitter, ByteBufferList bb) {
-        ByteBuffer buffer = ByteBuffer.allocate(bb.remaining());
-        while (bb.remaining() > 0) {
-            byte b = bb.get();
-            if (b == '\n') {
-                assert mLineCallback != null;
-                buffer.flip();
-                data.add(buffer);
-                mLineCallback.onStringAvailable(data.readString(charset));
-                data = new ByteBufferList();
-                return;
-            }
-            else {
-                buffer.put(b);
-            }
-        }
-        buffer.flip();
-        data.add(buffer);
+        String s = bb.readString();
+        if (s.indexOf("\n") > 0)
+            mLineCallback.onStringAvailable(s);
     }
 }
