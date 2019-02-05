f822551151d747b0cf7b370f4a3d5e6cdecc422f
Koushik Dutta <koushd@gmail.com>
2019-02-05 12:45:00 -0800

    Revert "seems i do need the content length"
    This reverts commit 3b5667080f4d20c021578a1f22aa8115d23cfd51.

--- AndroidAsync/src/com/koushikdutta/async/http/server/AsyncHttpServerRouter.java
+++ AndroidAsync/src/com/koushikdutta/async/http/server/AsyncHttpServerRouter.java
@@ -232,8 +232,6 @@ public class AsyncHttpServerRouter implements RouteMatcher {
                 response.end();
                 return;
             }
-            response.getHeaders().set("Content-Length", String.valueOf(pair.available));
-            response.getHeaders().add("Content-Type", getContentType(pair.path));
 
             if (isClientCached(context, request, response, pair.path)) {
                 StreamUtility.closeQuietly(pair.inputStream);
@@ -242,6 +240,9 @@ public class AsyncHttpServerRouter implements RouteMatcher {
                 response.end();
                 return;
             }
+
+            response.getHeaders().set("Content-Length", String.valueOf(pair.available));
+            response.getHeaders().add("Content-Type", getContentType(pair.path));
 
             response.code(200);
             Util.pump(pair.inputStream, pair.available, response, ex -> {
@@ -258,13 +259,16 @@ public class AsyncHttpServerRouter implements RouteMatcher {
                 return;
             }
             StreamUtility.closeQuietly(pair.inputStream);
-            response.getHeaders().set("Content-Length", String.valueOf(pair.available));
-            response.getHeaders().add("Content-Type", getContentType(pair.path));
 
-            if (isClientCached(context, request, response, pair.path))
+            if (isClientCached(context, request, response, pair.path)) {
                 response.code(304);
+            }
             else
+            {
+                response.getHeaders().set("Content-Length", String.valueOf(pair.available));
+                response.getHeaders().add("Content-Type", getContentType(pair.path));
                 response.code(200);
+            }
 
             response.writeHead();
             response.end();
